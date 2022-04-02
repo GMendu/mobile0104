18e16b4ae177153c7cdd2aba8d10ea2ad2b9a21f
gsbrile mendonda <bielm2000@gmail.com>
2022-04-02 00:42:20 -0300

    adicionado icone

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,162 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SwipeConfiguration {
+  //Vertical swipe configuration options
+  double verticalSwipeMaxWidthThreshold = 50.0;
+  double verticalSwipeMinDisplacement = 100.0;
+  double verticalSwipeMinVelocity = 300.0;
+  //Horizontal swipe configuration options
+  double horizontalSwipeMaxHeightThreshold = 50.0;
+  double horizontalSwipeMinDisplacement = 100.0;
+  double horizontalSwipeMinVelocity = 300.0;
+
+  SwipeConfiguration({
+    double? verticalSwipeMaxWidthThreshold,
+    double? verticalSwipeMinDisplacement,
+    double? verticalSwipeMinVelocity,
+    double? horizontalSwipeMaxHeightThreshold,
+    double? horizontalSwipeMinDisplacement,
+    double? horizontalSwipeMinVelocity,
+  }) {
+    if (verticalSwipeMaxWidthThreshold != null) {
+      this.verticalSwipeMaxWidthThreshold = verticalSwipeMaxWidthThreshold;
+    }
+    if (verticalSwipeMinDisplacement != null) {
+      this.verticalSwipeMinDisplacement = verticalSwipeMinDisplacement;
+    }
+    if (verticalSwipeMinVelocity != null) {
+      this.verticalSwipeMinVelocity = verticalSwipeMinVelocity;
+    }
+    if (horizontalSwipeMaxHeightThreshold != null) {
+      this.horizontalSwipeMaxHeightThreshold = horizontalSwipeMaxHeightThreshold;
+    }
+    if (horizontalSwipeMinDisplacement != null) {
+      this.horizontalSwipeMinDisplacement = horizontalSwipeMinDisplacement;
+    }
+    if (horizontalSwipeMinVelocity != null) {
+      this.horizontalSwipeMinVelocity = horizontalSwipeMinVelocity;
+    }
+  }
+}
+
+
+class SwipeDetector extends StatelessWidget {
+  final Widget child;
+  final Function() onSwipeUp;
+  final Function() onSwipeDown;
+  final Function() onSwipeLeft;
+  final Function() onSwipeRight;
+  final SwipeConfiguration swipeConfiguration;
+
+  SwipeDetector(
+      {required this.child,
+      required this.onSwipeUp,
+      required this.onSwipeDown,
+      required  this.onSwipeLeft,
+      required this.onSwipeRight,
+      SwipeConfiguration? swipeConfiguration})
+      : this.swipeConfiguration = swipeConfiguration == null
+            ? SwipeConfiguration()
+            : swipeConfiguration;
 
   @override
   Widget build(BuildContext context) {
+    //Vertical drag details
+    late DragStartDetails startVerticalDragDetails;
+    late DragUpdateDetails updateVerticalDragDetails;
+    //Horizontal drag details
+    late DragStartDetails startHorizontalDragDetails;
+    late DragUpdateDetails updateHorizontalDragDetails;
+    return GestureDetector(
+      child: child,
+      onVerticalDragStart: (dragDetails) {
+        startVerticalDragDetails = dragDetails;
+      },
+      onVerticalDragUpdate: (dragDetails) {
+        updateVerticalDragDetails = dragDetails;
+      },
+      onVerticalDragEnd: (endDetails) {
+        double dx = updateVerticalDragDetails.globalPosition.dx -
+            startVerticalDragDetails.globalPosition.dx;
+        double dy = updateVerticalDragDetails.globalPosition.dy -
+            startVerticalDragDetails.globalPosition.dy;
+        double velocity = endDetails.primaryVelocity!;
+        //Convert values to be positive
+        if (dx < 0) dx = -dx;
+        if (dy < 0) dy = -dy;
+        double positiveVelocity = velocity < 0 ? -velocity : velocity;
+        if (dx > swipeConfiguration.verticalSwipeMaxWidthThreshold) return;
+        if (dy < swipeConfiguration.verticalSwipeMinDisplacement) return;
+        if (positiveVelocity < swipeConfiguration.verticalSwipeMinVelocity)
+          return;
+        if (velocity < 0) {
+          //Swipe Up
+          if (onSwipeUp != null) {
+            onSwipeUp();
+          }
+        } else {
+          //Swipe Down
+          if (onSwipeDown != null) {
+            onSwipeDown();
+          }
+        }
+      },
+      onHorizontalDragStart: (dragDetails) {
+        startHorizontalDragDetails = dragDetails;
+      },
+      onHorizontalDragUpdate: (dragDetails) {
+        updateHorizontalDragDetails = dragDetails;
+      },
+      onHorizontalDragEnd: (endDetails) {
+        double dx = updateHorizontalDragDetails.globalPosition.dx -
+            startHorizontalDragDetails.globalPosition.dx;
+        double dy = updateHorizontalDragDetails.globalPosition.dy -
+            startHorizontalDragDetails.globalPosition.dy;
+        double velocity = endDetails.primaryVelocity!;
+        if (dx < 0) dx = -dx;
+        if (dy < 0) dy = -dy;
+        double positiveVelocity = velocity < 0 ? -velocity : velocity;
+        if (dx < swipeConfiguration.horizontalSwipeMinDisplacement) return;
+        if (dy > swipeConfiguration.horizontalSwipeMaxHeightThreshold) return;
+        if (positiveVelocity < swipeConfiguration.horizontalSwipeMinVelocity)
+          return;
+        if (velocity < 0) {
+          //Swipe Up
+          if (onSwipeLeft != null) {
+            onSwipeLeft();
+          }
+        } else {
+          //Swipe Down
+          if (onSwipeRight != null) {
+            onSwipeRight();
+          }
+        }
+      },
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Mulish',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Gerador de cores aleatórias'),
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -27,84 +165,77 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
-  int eixoX = 150;
-  int eixoY = 150;
+  int coordenadasX = 0;
+  int coordenadasY = 0;
+  Color? corCirculo = Colors.amber;
+
+  void CorRand() {
+    setState(() {
+      corCirculo = Colors.primaries[Random().nextInt(Colors.primaries.length)];
+    });
+  }
   void subir() {
     setState(() {
-      eixoX++;
+      coordenadasX++;
     });
   }
-
   void descer() {
     setState(() {
-      eixoX--;
+      coordenadasX--;
     });
   }
-
   void direita() {
     setState(() {
-      eixoY++;
+      coordenadasY++;
     });
   }
-
   void esquerda() {
     setState(() {
-      eixoY--;
+      coordenadasY--;
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body:Stack( children: [Positioned(
-        left: eixoX.toDouble(),
-        top: eixoY.toDouble(),
-        child: Card(
-          elevation: 10.0,
-          shape: const CircleBorder(),
-          child: Container(
-            width: 80.0,
-            height: 80.0,
-            decoration: const BoxDecoration(
-              color: Colors.amber,
-              shape: BoxShape.circle,
+      body:Center(
+          child: SwipeDetector(
+              child:Stack(
+                children: <Widget>[
+                  Container(
+                    child: Center(
+                      child: Text("$coordenadasX $coordenadasY")
+                      ),
+                     width: 600.0,
+                     height: 600.0,
+                     decoration: BoxDecoration(
+                        color: corCirculo,
+                        shape: BoxShape.circle,
             ),
           ),
-          ),
-      ),
-      ]
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          GestureDetector(
-            onPanUpdate: (details) {
-              if (details.delta.dx > 0) {
-                subir();
-              } else if (details.delta.dx < 0) {
-                descer();
-              } else if (details.delta.dy > 0) {
-                direita();
-              } 
-            },
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.blue[400],
-                shape: BoxShape.circle,
+              ]
               ),
-              child: const Icon(Icons.add),
+              onSwipeUp: () {
+                subir();
+                CorRand();
+              },
+              onSwipeDown: () { 
+                descer();
+                CorRand();
+              }, 
+              onSwipeRight: () {
+                direita();
+                CorRand();
+              }, 
+              onSwipeLeft: () {
+                esquerda();
+                CorRand();
+              },
             ),
           ),
-        ],
-      ),
-    );
+      );
   }
 }
